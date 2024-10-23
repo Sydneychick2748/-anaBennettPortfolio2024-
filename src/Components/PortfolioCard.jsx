9784c5f517cd6e2f75558b2db74db0eec28e2e01
ana <ana@anniecannons.com>
2024-10-23 09:57:10 -0700

    links

--- src/Components/PortfolioCard.jsx
+++ src/Components/PortfolioCard.jsx
@@ -1,10 +1,8 @@
 import "./PortfolioCard.scss";
 
-import React, { useEffect } from 'react';
-
+import React, { useEffect } from "react";
 
 const projects = [
-  
   {
     title: "Lighthouse App",
     description: `
@@ -14,9 +12,11 @@ const projects = [
       Integrated AWS Amplify for secure user authentication, managing login and password reset with form validation and error handling.
       Rebuilt the entire CSS from scratch to align with the new design, updating images, logos, and layout for a modern, consistent look.
     `,
-    // videoUrl: "./Videos/Lighthouse app.mp4"
-    videoUrl: "https://youtu.be/PERoi2a8dyw"
-   // https://youtu.be/PERoi2a8dyw
+    // videoUrl: "https://www.youtube.com/embed/PERoi2a8dyw"
+
+    videoUrl: "https://s3.us-west-1.amazonaws.com/a.bennettportfoilo/Lighthouse+app.mp4",
+    // videoUrl: "https://youtu.be/PERoi2a8dyw"
+    // https://youtu.be/PERoi2a8dyw
   },
   {
     title: "Kubecost Webpage",
@@ -27,12 +27,10 @@ const projects = [
       The project helps Kubecost users monitor and optimize their Kubernetes spending in real-time, 
       offering detailed views of resource usage and related costs.
     `,
-    // videoUrl: "/Videos/Kubecost.mp4"
-    videoUrl: "https://www.youtube.com/embed/Ls0V2jgmMyU"
+    videoUrl: "https://s3.us-west-1.amazonaws.com/a.bennettportfoilo/Kubecost.mp4",
     // videoUrl: "https://youtu.be/Ls0V2jgmMyU"
   },
-     //https://youtu.be/Ls0V2jgmMyU
-  
+  //https://youtu.be/Ls0V2jgmMyU
 
   {
     title: "Resourceful Provider App",
@@ -46,10 +44,8 @@ const projects = [
       - Displayed organization names with conditional text for virtual programs and confidential addresses.
       This project showcases my ability to integrate cloud-based data with React and handle dynamic, conditional rendering.
     `,
-    
-    // videoUrl: "./Videos/ResourceFull Providers.mp4"
-    videoUrl: "https://youtu.be/kZHsLRCEHog"
-   
+    // videoUrl: "https://www.youtube.com/embed/v7kqrDBzBXY"
+    videoUrl: "https://s3.us-west-1.amazonaws.com/a.bennettportfoilo/ResourceFull+Providers.mp4",
   },
   {
     title: "2024 Subaru Crosstrek Landing Page",
@@ -58,58 +54,43 @@ const projects = [
       The project was completed in 2 hours using Bootstrap and jQuery, with a focus on responsiveness from 360 pixels and up. 
       All assets and text were provided, and I implemented a design that fulfilled all specified criteria.
     `,
-     videoUrl: "https://youtu.be/v7kqrDBzBXY"
-   
-    
-    // videoUrl: "https://player.vimeo.com/video/1018508906" 
-    // videoUrl: "./Videos/2024 Subaru Crosstrek Landing Page.mp4"
-    // https://youtu.be/v7kqrDBzBXY
-  }
+    // videoUrl: "https://www.youtube.com/embed/v7kqrDBzBXY"
+    //videoUrl: "https://player.vimeo.com/video/1020360541"
+    videoUrl: "https://s3.us-west-1.amazonaws.com/a.bennettportfoilo/2024+Subaru+Crosstrek+Landing+Page.mp4",
+  },
 ];
 
-
 const PortfolioCard = () => {
-  
-    return (
-      <div className="portfolio-container">
-        <div className="portfolio-grid">
-          {projects.map((project, index) => (
-            <div key={index} className="project-card">
-              {/* <div className="card-image">
+  return (
+    <div className="portfolio-container">
+      <div className="portfolio-grid">
+        {projects.map((project, index) => (
+          <div key={index} className="project-card">
+            {/* <div className="card-image">
                 <img src={project.imageUrl} alt={`${project.title} Screenshot`} />
               </div> */}
-              <div className="card-info">
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                
-                {/* <video controls width="100%">
-      <source src={project.videoUrl} type="video/mp4" />
-      Your browser does not support the video tag.
-    </video> */}
-    <iframe
-              width="100%"
-              height="500"
-              src={project.videoUrl}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title={project.title}
-            ></iframe>
-    
-    {/* <iframe 
-                width="100%" 
-                height="500" 
-                src={`${project.videoUrl.replace('youtu.be', 'www.youtube.com/embed')}?rel=0`} 
-                frameBorder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowFullScreen>
-              </iframe> */}
-              </div>
+            <div className="card-info">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+
+              <video autoPlay muted loop width="100%">
+                <source src={project.videoUrl} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+              {/* <iframe 
+  width="100%" 
+  height="500" 
+  src={project.videoUrl} 
+  frameBorder="0" 
+  allow="autoplay; fullscreen; picture-in-picture" 
+  allowFullScreen>
+</iframe> */}
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default PortfolioCard;
